842aa27ff099f449b2c00be2d474ce316d243694
Gelson Sousa Feitoza <71074284+GelsonSF@users.noreply.github.com>
2022-02-25 17:05:09 -0300

    Coleta de dados API - Pokemon

--- Leitura_Arquivos/Coleta-Dados-API-WEB-Pokemon.py
+++ Leitura_Arquivos/Coleta-Dados-API-WEB-Pokemon.py
@@ -1,23 +1,25 @@
 import pandas as pd
 import requests
 
-pokemons = []
-    
+infos = {}
+
 def extract_pokemons_name():
-    for i in range(1, 30):
+    for i in range(1, 11):
         api_r = f'https://pokeapi.co/api/v2/pokemon/{i}/'
         resp = requests.get(api_r)
         requ = resp.json()
-        
+
         for i in requ['forms']:
-            pokemons.append(i['name'])
-        for i in requ['types']:
-            pokemons.append(i['type']['name'])
-            
-    df = pd.DataFrame(pokemons)
+          for j in requ['types']:
+            infos[i['name']] = j['type']['name']
+
+        df = pd.DataFrame([infos])
+        df = df.set_index(i['name'])
+        df = df.T
+
     display(df)
 	
 def main():
-    extract_pokemons_name()
-	
+	extract_pokemons_name()
+
 main()
