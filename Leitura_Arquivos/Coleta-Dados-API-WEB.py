2f35966428247007827ae6b5014d0ab0bc19eba2
Gelson Sousa Feitoza <71074284+GelsonSF@users.noreply.github.com>
2022-02-25 07:48:20 -0300

    Update Coleta-Dados-API-WEB.py

--- Leitura_Arquivos/Coleta-Dados-API-WEB.py
+++ Leitura_Arquivos/Coleta-Dados-API-WEB.py
@@ -2,7 +2,6 @@ import requests
 import pandas as pd
 
 #### Preparação para a extração dos nomes de pokemon ####
-
 try:
     url_name = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1200'
     res_name = requests.get(url_name)
@@ -13,7 +12,6 @@ except Exception as e:
     print(f'Problema ao se conectar a API! {e}')
 
 #### Extração dos nomes de pokemon, inclusão em uma lista e transformação em Dataframe. ####
-
 try:
     pokemons = []
     
@@ -30,11 +28,9 @@ except Exception as e:
     print(f'Não foi possível realizar a extração! {e}')
 
 #### Demostrativo da lista com os nomes dos pokemons ####
-
 display(pokemons_names)
 
 #### Criação de uma função para buscar informações dos pokemons, percorrendo uma lista. ####
-
 def busca_pokemon(pokemons_names):
     try:
         api = f'https://pokeapi.co/api/v2/pokemon/{pokemons_names}'
@@ -51,7 +47,6 @@ def busca_pokemon(pokemons_names):
 busca_pokemon(pokemons_names['Name'][1])
 
 #### Busca das informações dos 3 primeiros pokemons da lista, e load para  ####
-
 try:
     info_pokemons = []
 
